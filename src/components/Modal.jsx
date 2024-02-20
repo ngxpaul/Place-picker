6b4f37e10ad1bb81e4e0cfefb9f3815a7784d940
ngxpaul <phongnx.22git@vku.udn.vn>
2024-02-20 07:05:07 +0700

    182

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,25 +1,31 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const Modal = forwardRef(function Modal({ children }, ref) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
-
+  // useImperativeHandle(ref, () => {
+  //   return {
+  //     open: () => {
+  //       dialog.current.showModal();
+  //     },
+  //     close: () => {
+  //       dialog.current.close();
+  //     },
+  //   };
+  // });
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, []);
   return createPortal(
     <dialog className="modal" ref={dialog}>
       {children}
     </dialog>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 });
 
